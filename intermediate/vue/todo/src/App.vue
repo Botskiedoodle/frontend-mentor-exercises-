adba140bc37ffe81dfa83584b83089c19ca6f10c
Botskiedoodle <tanfelix.richard@gmail.com>
2023-10-19 14:31:09 +0800

    draggable

--- intermediate/vue/todo/src/App.vue
+++ intermediate/vue/todo/src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Favorite Foods</h1>
+    <draggable v-model="favoriteFoods" tag="ul">
+      <template #item="{ element: food }">
+        <li style="cursor: move">{{ food.text }}</li>
+      </template>
+    </draggable>
+
+
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from 'vue'
+import draggable from 'vuedraggable'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const favoriteFoods = ref([
+  {
+    id: 1,
+    text: 'Hamburger'
+  },
+  {
+    id: 2,
+    text: 'Pizza'
+  },
+  {
+    id: 3,
+    text: 'Spaghetti'
+  },
+  {
+    id: 4,
+    text: 'Tacos'
+  },
+  {
+    id: 5,
+    text: 'Teriyaki Chicken'
   }
-}
+])
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
